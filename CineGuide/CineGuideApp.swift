cff25bcfba76ae90d2dbaaddfd33c7e58feec107
JeremyKrzeczowski <jeremy.krzeczowski77@gmail.com>
2024-09-09 14:34:09 +0200

    Initial Commit

---
+++ CineGuide/CineGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineGuideApp.swift
+//  CineGuide
+//
+//  Created by etudiant on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CineGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineGuide
+//
+//  Created by etudiant on 09/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
